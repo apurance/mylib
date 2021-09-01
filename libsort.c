b5bb965fd88548e9220ace0aaf812afd46fc5f96
apurance <apurance@gmail.com>
2021-09-01 22:47:21 +0800

    add mergesort and insertsort for libsort

--- libsort.c
+++ libsort.c
@@ -1,5 +1,8 @@
 #include "libsort.h"
 #include <stdint.h>
+#include <string.h>
+
+#define TMP_SIZE 128
 
 static size_t g_type_size;
 
@@ -71,11 +74,10 @@ fn_swap swap_check(size_t type_size)
 }
 
 void select_sort(void *array, size_t len, size_t type_size,
-                 fn_cmp cmp,
-                 fn_swap swap)
+                 fn_cmp cmp, fn_swap swap)
 {
     uint8_t *i, *j, *k;
-    uint8_t *end = array + (len - 1) * type_size;
+    uint8_t *end = (uint8_t *)array + (len - 1) * type_size;
 
     if (!swap){
         swap = swap_check(type_size);
@@ -95,15 +97,91 @@ void select_sort(void *array, size_t len, size_t type_size,
 }
 
 void quick_sort(void *array, size_t len, size_t type_size,
-                 fn_cmp cmp,
-                 fn_swap swap)
+                fn_cmp cmp, fn_swap swap)
 {
     // TODO
 }
 
+void insert_sort(void *array, size_t len, size_t type_size,
+                 fn_cmp cmp, void *tmp)
+{
+    uint8_t *i, *j;
+    uint8_t *end = (uint8_t *)array + (len - 1) * type_size;
+    
+    for (i = (uint8_t *)array + type_size; i <= end; i += type_size){
+        // back search
+        for (j = i - type_size; j >= (uint8_t *)array; j -= type_size){
+            if (cmp(i, j) >= 0){
+                break;
+            }
+        }
+        j += type_size;
+        // insert
+        if (i > j){
+            memcpy(tmp, i, type_size);
+            memmove(j + type_size, j, i - j);
+            memcpy(j, tmp, type_size);
+        }
+    }
+}
+
+static
+void _merge(void *array, void *aux, size_t low, size_t mid, size_t high, size_t type_size, fn_cmp cmp)
+{
+    uint8_t *i = (uint8_t *)aux + low * type_size;
+    uint8_t *j = (uint8_t *)aux + (mid + 1) * type_size;
+    uint8_t *midp = (uint8_t *)aux + mid * type_size;
+    uint8_t *highp = (uint8_t *)aux + high * type_size;
+    uint8_t *k = (uint8_t *)array + low * type_size;
+    uint8_t *end = (uint8_t *)array + high * type_size;
+
+    for (; k <= end; k += type_size){
+        if (i > midp){
+            memcpy(k, j, end - k + type_size);
+            break;
+        }
+        if (j > highp){
+            memcpy(k, i, end - k + type_size);
+            break;
+        }
+        if (cmp(i, j) > 0){
+            memcpy(k, j, type_size);
+            j += type_size;
+        }
+        else{
+            memcpy(k, i, type_size);
+            i += type_size;
+        }
+    }
+}
+
+static
+void _merge_sort_recur(void *array, void *aux, size_t low, size_t high,size_t type_size, fn_cmp cmp)
+{
+    if (low >= high)
+        return;
+    size_t mid = low + ((high - low) >> 1);
+    _merge_sort_recur(aux, array, low, mid, type_size, cmp);
+    _merge_sort_recur(aux, array, mid + 1, high, type_size, cmp);
+    if (cmp((uint8_t *)aux + mid * type_size, (uint8_t *)aux + (mid + 1) * type_size) <= 0){
+        memcpy((uint8_t *)array + low * type_size, (uint8_t *)aux + low * type_size, (high - low + 1) * type_size);
+    }
+    else{
+        _merge(array, aux, low, mid, high, type_size, cmp);
+    }
+}
+
+void merge_sort(void *array, size_t len, size_t type_size,
+                fn_cmp cmp, void *aux)
+{
+    memcpy(aux, array, len * type_size);
+    _merge_sort_recur(array, aux, 0, len - 1, type_size, cmp);
+}
+
+
 void shuffle(void *array, size_t len, size_t type_size,
-                 size_t (*randint)(size_t bound),
-                 fn_swap swap)
+             size_t (*randint)(size_t bound),
+             fn_swap swap)
 {
     uint8_t *i = array, *j;
     uint8_t *end = (uint8_t *)array + (len - 1) * type_size;
--- libsort.h
+++ libsort.h
@@ -7,11 +7,18 @@ typedef void (*fn_swap)(void *x, void *y);
 typedef int (*fn_cmp)(void *x, void *y);
 
 void select_sort(void *array, size_t len, size_t type_size,
-                 fn_cmp cmp,
-                 fn_swap swap);
+                 fn_cmp cmp, fn_swap swap);
 void quick_sort(void *array, size_t len, size_t type_size,
-                 fn_cmp cmp,
-                 fn_swap swap);
+                fn_cmp cmp, fn_swap swap);
+
+/* 
+ * @param tmp must not be NULL, its size should at least >= type_size
+ */
+void insert_sort(void *array, size_t len, size_t type_size,
+                 fn_cmp cmp, void *tmp);
+
+void merge_sort(void *array, size_t len, size_t type_size,
+                fn_cmp cmp, void *aux);
 void shuffle(void *array, size_t len, size_t type_size,
                  size_t (*randint)(size_t bound),
                  fn_swap swap);
