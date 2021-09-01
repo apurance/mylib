d333066546756571c524165af972b8f0a94003b0
apurance <apurance@gmail.com>
2021-09-01 15:02:13 +0800

    add shuffle for libsort

--- libsort.c
+++ libsort.c
@@ -17,6 +17,24 @@ void general_swap(void *x, void *y)
 }
 
 static inline
+void u8_swap(void *x, void *y)
+{
+    uint8_t tmp;
+    tmp = *(uint8_t *)x;
+    *(uint8_t *)x = *(uint8_t *)y;
+    *(uint8_t *)y = tmp;
+}
+
+static inline
+void u16_swap(void *x, void *y)
+{
+    uint16_t tmp;
+    tmp = *(uint16_t *)x;
+    *(uint16_t *)x = *(uint16_t *)y;
+    *(uint16_t *)y = tmp;
+}
+
+static inline
 void u32_swap(void *x, void *y)
 {
     uint32_t tmp;
@@ -25,36 +43,77 @@ void u32_swap(void *x, void *y)
     *(uint32_t *)y = tmp;
 }
 
+static inline
+void u64_swap(void *x, void *y)
+{
+    uint64_t tmp;
+    tmp = *(uint64_t *)x;
+    *(uint64_t *)x = *(uint64_t *)y;
+    *(uint64_t *)y = tmp; 
+}
+
+static inline
+fn_swap swap_check(size_t type_size)
+{
+    switch(type_size){
+        case 1:
+            return u8_swap;
+        case 2:
+            return u16_swap;
+        case 4:
+            return u32_swap;
+        case 8:
+            return u64_swap;
+        default:
+            g_type_size = type_size;
+            return general_swap;
+    }
+}
+
 void select_sort(void *array, size_t len, size_t type_size,
-                 int (*fn_cmp)(void *x, void *y),
-                 void (*fn_swap)(void *x, void *y))
+                 fn_cmp cmp,
+                 fn_swap swap)
 {
     uint8_t *i, *j, *k;
     uint8_t *end = array + (len - 1) * type_size;
 
-    if (!fn_swap){
-        switch (type_size)
-        {
-        case 4:
-            fn_swap = u32_swap;
-            break;
-        
-        default:
-            g_type_size = type_size;
-            fn_swap = general_swap;
-            break;
-        }
+    if (!swap){
+        swap = swap_check(type_size);
     }
 
     for (i = array; i < end; i += type_size){
         k = i;
         for (j = i + type_size; j <= end; j += type_size){
-            if (fn_cmp(k, j) > 0){
+            if (cmp(k, j) > 0){
                 k = j;
             }
         }
         if (k != i){
-            fn_swap(k, i);
+            swap(k, i);
         }
+    }
+}
+
+void quick_sort(void *array, size_t len, size_t type_size,
+                 fn_cmp cmp,
+                 fn_swap swap)
+{
+    // TODO
+}
+
+void shuffle(void *array, size_t len, size_t type_size,
+                 size_t (*randint)(size_t bound),
+                 fn_swap swap)
+{
+    uint8_t *i = array, *j;
+    uint8_t *end = (uint8_t *)array + (len - 1) * type_size;
+
+    if (!swap){
+        swap = swap_check(type_size);
+    }
+
+    for (; i < end; i += type_size){
+        j = i + randint(len--) * type_size;
+        swap(i, j);
     }
 }
--- libsort.h
+++ libsort.h
@@ -3,8 +3,17 @@
 
 #include <stddef.h>
 
+typedef void (*fn_swap)(void *x, void *y);
+typedef int (*fn_cmp)(void *x, void *y);
+
 void select_sort(void *array, size_t len, size_t type_size,
-                 int (*fn_cmp)(void *x, void *y),
-                 void (*fn_swap)(void *x, void *y));
+                 fn_cmp cmp,
+                 fn_swap swap);
+void quick_sort(void *array, size_t len, size_t type_size,
+                 fn_cmp cmp,
+                 fn_swap swap);
+void shuffle(void *array, size_t len, size_t type_size,
+                 size_t (*randint)(size_t bound),
+                 fn_swap swap);
 
 #endif
